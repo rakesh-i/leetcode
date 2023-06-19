31c78add61381564e1c4bd52e21d301df61a350d
Rakesh <rakesh.007.ac@gmail.com>
2023-06-19 16:43:50 +0530

    20/20 cases passed (0 ms) Your runtime beats 100 % of cpp submissions Your memory usage beats 60.15 % of cpp submissions (6.5 MB)

---
+++ 59.spiral-matrix-ii.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=59 lang=cpp
+ *
+ * [59] Spiral Matrix II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        int count = 1;
+        vector<vector<int>> a(n, vector<int> (n));
+        int l = 0  , r = n-1, t = 0, b = n-1;
+        while(l<=r&&t<=b){
+            for(int i=l; i<=r; i++){
+                a[t][i] = count++;
+            }
+            t++;
+            for(int i=t; i<=b; i++){
+                a[i][r] = count++;
+            }
+            r--;
+            if(t<=b){
+                for(int i=r; i>=l; i--){
+                    a[b][i] =count++;
+                }
+                b--;
+            }
+            if(l<=r){
+                for(int i=b; i>=t; i--){
+                    a[i][l]  =  count++;
+                }
+                l++;
+            }
+        }
+        return a;  
+    }
+};
+// @lc code=end
+
