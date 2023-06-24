d135c1c1718e53e1ea71d7140cd1ec4387d8bc80
Rakesh <rakesh.007.ac@gmail.com>
2023-06-24 19:21:10 +0530

    66/66 cases passed (4 ms) Your runtime beats 77.45 % of cpp submissions Your memory usage beats 9.81 % of cpp submissions (8.9 MB)

---
+++ 162.find-peak-element.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=162 lang=cpp
+ *
+ * [162] Find Peak Element
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int low = 1;
+        int high = n-2;
+        if(n==1){
+            return 0;
+        }
+        if(nums[0]>nums[1]){
+            return 0;
+        }
+        if(nums[n-1]>nums[n-2]){
+            return n-1;
+        }
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]>nums[mid+1]&&nums[mid]>nums[mid-1]){
+                return mid;
+            }
+            else if(nums[mid]<nums[mid-1]){
+                high = mid-1;
+            }
+            else if(nums[mid]<nums[mid+1]){
+                low = mid+1;
+            }
+        }
+        return 0;
+    }
+};
+// @lc code=end
+
