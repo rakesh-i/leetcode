8cebc22d2bca2cad7df35cc7be0bea9d305e2afa
Rakesh <rakesh.007.ac@gmail.com>
2023-06-24 14:20:21 +0530

    195/195 cases passed (3 ms) Your runtime beats 79.35 % of cpp submissions Your memory usage beats 52.3 % of cpp submissions (11.1 MB)

---
+++ 33.search-in-rotated-sorted-array.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=33 lang=cpp
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n= nums.size();
+        int lo = 0; int hi = n-1;
+        while(hi>=lo){
+            int mid = (hi+lo)/2;
+            double num = (nums[mid]<nums[0])==(target<nums[0])?
+                        nums[mid]
+                        :target<nums[0]?-INFINITY:INFINITY;
+            if(num<target){
+                lo = mid+1;
+            }
+            else if(num>target){
+                hi = mid-1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
+// @lc code=end
+
