3525608723feef596dd1cdc970c8907908523d0f
Rakesh <rakesh.007.ac@gmail.com>
2023-06-29 21:27:04 +0530

    588/588 cases passed (0 ms) Your runtime beats 100 % of cpp submissions Your memory usage beats 100 % of cpp submissions (5.8 MB)

---
+++ 279.perfect-squares.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=279 lang=cpp
+ *
+ * [279] Perfect Squares
+ */
+
+// @lc code=start
+class Solution { 
+public:
+    int is_square(int n)
+    {  
+        int sqrt_n = (int)(sqrt(n));  
+        return (sqrt_n*sqrt_n == n);  
+    }
+    int numSquares(int n) 
+    {  
+        // If n is a perfect square, return 1.
+        if(is_square(n)) 
+        {
+            return 1;  
+        }
+        
+        // The result is 4 if and only if n can be written in the 
+        // form of 4^k*(8*m + 7). Please refer to 
+        // Legendre's three-square theorem.
+        while ((n & 3) == 0) // n%4 == 0  
+        {
+            n >>= 2;  
+        }
+        if ((n & 7) == 7) // n%8 == 7
+        {
+            return 4;
+        }
+        
+        // Check whether 2 is the result.
+        int sqrt_n = (int)(sqrt(n)); 
+        for(int i = 1; i <= sqrt_n; i++)
+        {  
+            if (is_square(n - i*i)) 
+            {
+                return 2;  
+            }
+        }  
+        
+        return 3;  
+    }  
+};
+// @lc code=end
+
