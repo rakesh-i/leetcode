f6e3ff588adb6c3c454e01310088908e54339a50
Rakesh <rakesh.007.ac@gmail.com>
2023-07-03 16:53:22 +0530

    15/15 cases passed (28 ms) Your runtime beats 72.52 % of cpp submissions Your memory usage beats 33.83 % of cpp submissions (22.3 MB)

---
+++ 540.single-element-in-a-sorted-array.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=540 lang=cpp
+ *
+ * [540] Single Element in a Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size()-1;
+        while(low<high){
+            int mid = (low+high)/2;
+            if(mid%2==0&&nums[mid]==nums[mid+1]||mid%2==1&&nums[mid]==nums[mid-1]){
+                low = mid+1;
+            }
+            else{
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+};
+// @lc code=end
+
