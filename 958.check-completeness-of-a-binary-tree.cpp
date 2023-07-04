a75ed500c22a4be93ce56ce8b4968ce91ecce146
Rakesh <rakesh.007.ac@gmail.com>
2023-07-04 22:33:43 +0530

    120/120 cases passed (3 ms) Your runtime beats 83.71 % of cpp submissions Your memory usage beats 32.59 % of cpp submissions (10.5 MB)

---
+++ 958.check-completeness-of-a-binary-tree.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=958 lang=cpp
+ *
+ * [958] Check Completeness of a Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        vector<TreeNode*> bfs;
+        bfs.push_back(root);
+        int i = 0;
+        while (i < bfs.size() && bfs[i]) {
+            bfs.push_back(bfs[i]->left);
+            bfs.push_back(bfs[i]->right);
+            i++;
+        }
+        while (i < bfs.size() && !bfs[i])
+            i++;
+        return i == bfs.size();
+    }
+};
+// @lc code=end
+
