a73573c54b35c8a52debd718e15851e3f1a13af7
rakesh-i <rakesh.007.ac@gmail.com>
2023-07-18 14:12:12 +0530

    57/57 cases passed (139 ms) Your runtime beats 96.55 % of csharp submissions Your memory usage beats 5.12 % of csharp submissions (46.9 MB)

---
+++ 1.two-sum.cs
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=1 lang=csharp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Hashtable m = new Hashtable();
+        int n = nums.Length;
+        int[] res = new int[2];
+        for(int i=0; i<n; i++){
+            if(m.ContainsKey(target-nums[i])){
+                res[0] = i;
+                res[1] =(int) m[target-nums[i]];
+                return res;
+            }
+            try{
+                m.Add(nums[i], i);
+            }
+            catch(Exception e){
+                continue;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
