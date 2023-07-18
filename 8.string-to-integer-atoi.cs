8969c2501c3d10fc7c04bad10906ed9eb411c838
rakesh-i <rakesh.007.ac@gmail.com>
2023-07-18 16:01:15 +0530

    1084/1084 cases passed (72 ms) Your runtime beats 53.53 % of csharp submissions Your memory usage beats 91.25 % of csharp submissions (38.8 MB)

---
+++ 8.string-to-integer-atoi.cs
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=8 lang=csharp
+ *
+ * [8] String to Integer (atoi)
+ */
+
+// @lc code=start
+public class Solution {
+    public int MyAtoi(string s) {
+        const int maxCmp = int.MaxValue / 10;
+        var num = 0;
+        var sign = 1;
+
+        var idx = 0;
+        while (idx < s.Length && s[idx] == ' ')
+            idx++;
+
+        if (idx < s.Length && (s[idx] == '+' || s[idx] == '-'))
+            sign = s[idx++] == '-' ? -1 : 1;
+
+        while (idx < s.Length && s[idx] > 47 && s[idx] < 58)
+        {
+            if (num > maxCmp || (num == maxCmp && (s[idx] - '0') > 7))
+                return sign == -1 ? int.MinValue : int.MaxValue;
+            num = num * 10 + (s[idx++] - '0');
+        }
+
+        return num * sign;
+    }
+}
+// @lc code=end
+
