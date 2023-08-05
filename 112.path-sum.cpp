60d61939da0510bbffb12819e9914f8645e95039
rakesh-i <rakesh.007.ac@gmail.com>
2023-08-05 19:47:56 +0530

    117/117 cases passed (9 ms) Your runtime beats 69.85 % of cpp submissions Your memory usage beats 41.94 % of cpp submissions (21.3 MB)

---
+++ 112.path-sum.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=112 lang=cpp
+ *
+ * [112] Path Sum
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* head, int n, int s, int* res){
+        if(!head||*res==1){
+            return;
+        }
+        s += head->val;
+        if(!head->left&&!head->right){
+            if(s==n){
+                *res = 1;
+            }
+            return;
+        }
+       
+        dfs(head->left, n, s, res);
+        dfs(head->right, n, s, res);
+        s = s-head->val;
+
+    }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        int res = 0;
+        dfs(root, targetSum, 0, &res);
+        if(res==1){
+            return true;
+        }
+        return false;
+    }
+};
+// @lc code=end
+
