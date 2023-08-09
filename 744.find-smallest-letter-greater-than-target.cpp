8191d725365a9b554b7039216b42a678821b550a
rakesh-i <rakesh.007.ac@gmail.com>
2023-08-09 16:59:04 +0530

    167/167 cases passed (18 ms) Your runtime beats 48.62 % of cpp submissions Your memory usage beats 67.7 % of cpp submissions (15.9 MB)

--- 744.find-smallest-letter-greater-than-target.cpp
+++ 744.find-smallest-letter-greater-than-target.cpp
@@ -9,14 +9,18 @@ class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
         int n = letters.size();
-        int y = target-'a';
-        for(int i=0; i<n; i++){
-            int x = letters[i]-'a';
-            if(x>y){
-                return letters[i];
+        int i=0; 
+        int j=n-1;
+        while(i<=j){
+            int mid = (j+i)/2;
+            if(letters[mid]>target){
+                j=mid-1;
+            }
+            else{
+                i=mid+1;
             }
         }
-        return letters[0];
+        return i == n?letters[0]:letters[i];
     }
 };
 // @lc code=end
