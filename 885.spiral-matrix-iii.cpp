c57ecbbbc53feb81d898228db549a9366b81cf98
rakesh-i <rakesh.007.ac@gmail.com>
2023-08-15 15:46:38 +0530

    73/73 cases passed (9 ms) Your runtime beats 93.33 % of cpp submissions Your memory usage beats 72.08 % of cpp submissions (12.1 MB)

--- 885.spiral-matrix-iii.cpp
+++ 885.spiral-matrix-iii.cpp
@@ -8,54 +8,59 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        vector<vector<int>> result;
-        int total = 0;
+        int c = 0;
+        int rs = rStart;
+        int re = rStart;
 
-        int rowStart = rStart;
-        int rowEnd = rStart;
-
-        int colStart = cStart;
-        int colEnd = cStart;
-
+        int cs = cStart;
+        int ce = cStart;
+        vector<vector<int>> res;
+        int n = rows*cols;
         while(true){
-            for(int i=colStart; i<=colEnd; i++){
-                if(i >= 0 && i < cols && rowStart >= 0){
-                    result.push_back({rowStart, i});
-                    total++;
+            for(int i=cs; i<=ce; i++){
+                if(i>=0 && i<cols&& rs>=0){
+                    res.push_back({rs, i});
+                    c++;
                 }
             }
-            colEnd++;
-            if(total == (rows * cols))
+            ce++;
+            if(c==n){
                 break;
-            for(int i=rowStart; i<=rowEnd; i++){
-                if(i >= 0 && i < rows && colEnd < cols){
-                    result.push_back({i, colEnd});
-                    total++;
+            }
+            for(int i=rs; i<=re; i++){
+                if(i>=0 && i<rows&& ce<cols){
+                    res.push_back({i,ce});
+                    c++;
                 }
             }
-            rowEnd++;
-            if(total == (rows * cols))
+            re++;
+            if(c==n){
                 break;
-            for(int i=colEnd; i>=colStart; i--){
-                if(i >= 0 && i < cols && rowEnd < rows){
-                    result.push_back({rowEnd, i});
-                    total++;
+            }
+            for(int i=ce; i>=cs; i--){
+                if(i>=0 && i<cols&& re<rows){
+                    res.push_back({re,i});
+                    c++;
                 }
             }
-            colStart--;
-            if(total == (rows * cols))
+            cs--;
+            if(c==n){
                 break;
-            for(int i=rowEnd; i>=rowStart; i--){
-                if(i >= 0 && i < rows && colStart >= 0){
-                    result.push_back({i, colStart});
-                    total++;
+            }
+            for(int i=re; i>=rs; i--){
+                if(i>=0 && i<rows&& cs>=0){
+                    res.push_back({i,cs});
+                    c++;
                 }
             }
-            rowStart--;
-            if(total == (rows * cols))
+            rs--;
+            if(c==n){
                 break;
+            }
+
+
         }
-        return result;
+        return res;
     }
 };
 // @lc code=end
