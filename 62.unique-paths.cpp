430b83063a86f13d48b8752a5c3e6cbf7ca4274b
rakesh-i <rakesh.007.ac@gmail.com>
2023-08-22 13:04:10 +0530

    63/63 cases passed (0 ms) Your runtime beats 100 % of cpp submissions Your memory usage beats 77.34 % of cpp submissions (6.1 MB)

--- 62.unique-paths.cpp
+++ 62.unique-paths.cpp
@@ -7,13 +7,18 @@
 // @lc code=start
 class Solution {
 public:
-    int dp[101][101]{};
-    int uniquePaths(int m, int n, int i = 0, int j = 0) {
-        if(i >= m || j >= n) return 0;
-        if(i == m-1 && j == n-1) return 1;
-        if(dp[i][j]) return dp[i][j];
-        return dp[i][j] = uniquePaths(m, n, i+1, j) + uniquePaths(m, n, i, j+1);
+    int l[101][101] = {0};
+    int uniquePaths(int m, int n) {
+        if(m==1||n==1){
+            return 1;
+        }
+        if(l[m][n]!=0){
+            return l[m][n];
+        }
+        l[m][n] = uniquePaths(m-1, n)+uniquePaths(m, n-1);
+        return l[m][n];
     }
 };
+        
 // @lc code=end
 
