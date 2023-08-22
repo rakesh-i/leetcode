647cdc3d23d940b744a5ee55c98e4f0f7fe9c0ee
rakesh-i <rakesh.007.ac@gmail.com>
2023-08-22 14:09:20 +0530

    41/41 cases passed (0 ms) Your runtime beats 100 % of cpp submissions Your memory usage beats 33.34 % of cpp submissions (7.9 MB)

--- 63.unique-paths-ii.cpp
+++ 63.unique-paths-ii.cpp
@@ -7,8 +7,27 @@
 // @lc code=start
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        
+    int path(vector<vector<int>>& o, int m, int n,vector<vector<int>>&l){
+        if(m>=0 && n>=0 && o[m][n]==1){
+            return 0;
+        }
+        if(m<0||n<0){
+            return 0;
+        }
+        if(m==0&&n==0){
+            return 1;
+        }
+        if(l[m][n]!=-1){
+            return l[m][n];
+        }
+        l[m][n] = path(o, m-1, n, l)+path(o, m, n-1, l);
+        return l[m][n];
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
+        int m = Grid.size();
+        int n = Grid[0].size();
+        vector<vector<int>> l(m+1,vector<int> (n+1,-1));
+        return path(Grid, m-1, n-1,l);
     }
 };
 // @lc code=end
