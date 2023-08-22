2fe0f9633d657cd1eb4e56fb427b5050f82aadc4
rakesh-i <rakesh.007.ac@gmail.com>
2023-08-22 12:16:06 +0530

    31/31 cases passed (0 ms) Your runtime beats 100 % of cpp submissions Your memory usage beats 24.96 % of cpp submissions (6 MB)

--- 509.fibonacci-number.cpp
+++ 509.fibonacci-number.cpp
@@ -7,14 +7,27 @@
 // @lc code=start
 class Solution {
 public:
+    int fib(int n, vector<int> &l){
+        if(l[n]!=-1){
+            return l[n];
+        }
+        if(n<0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        
+        l[n] = fib(n-1,l)+fib(n-2,l);
+        return l[n];
+
+    }
     int fib(int n) {
-       if(n<0){
-        return 0;
-       } 
-       if(n==1){
-        return 1;
-       }
-       return fib(n-1)+fib(n-2);
+       vector<int> l(31, -1);
+        l[0] = 0;
+        l[1] = 1;
+    //    return 0;
+       return fib(n, l);
     }
 };
 // @lc code=end
