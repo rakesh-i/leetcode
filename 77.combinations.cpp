79992dad9ae36e8cacf6b26b96d51861bfb84186
rakesh-i <rakesh.007.ac@gmail.com>
2023-08-26 11:18:59 +0530

    27/27 cases passed (318 ms) Your runtime beats 12.78 % of cpp submissions Your memory usage beats 11.71 % of cpp submissions (155.5 MB)

--- 77.combinations.cpp
+++ 77.combinations.cpp
@@ -8,22 +8,23 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    void helper(vector<int> &num, vector<int> temp, int idx, int n, int k){
+    void helper(int num, vector<int> temp, int idx, int n, int k){
         if(temp.size()==k){
             res.push_back(temp);
             return;
         }
         for(int j=idx; j<n; j++){
-            temp.push_back(num[j]);
-            helper(num, temp, j+1, n, k);
+            temp.push_back(j+1);
+            helper(j+1, temp, j+1, n, k);
             temp.pop_back();
         }
     }
     vector<vector<int>> combine(int n, int k) {
-        vector<int>num;
-        for(int i=0; i<n; i++){
-            num.push_back(i+1);
-        }
+        // vector<int>num;
+        // for(int i=0; i<n; i++){
+        //     num.push_back(i+1);
+        // }
+        int num = 1;
         vector<int> temp;
         helper(num, temp, 0, n, k);
         return res;
