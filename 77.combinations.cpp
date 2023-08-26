ad66f250fdc4929d0473174afb3edbf6350fa75d
rakesh-i <rakesh.007.ac@gmail.com>
2023-08-26 11:14:18 +0530

    27/27 cases passed (382 ms) Your runtime beats 10.45 % of cpp submissions Your memory usage beats 11.48 % of cpp submissions (155.6 MB)

---
+++ 77.combinations.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=77 lang=cpp
+ *
+ * [77] Combinations
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> res;
+    void helper(vector<int> &num, vector<int> temp, int idx, int n, int k){
+        if(temp.size()==k){
+            res.push_back(temp);
+            return;
+        }
+        for(int j=idx; j<n; j++){
+            temp.push_back(num[j]);
+            helper(num, temp, j+1, n, k);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>num;
+        for(int i=0; i<n; i++){
+            num.push_back(i+1);
+        }
+        vector<int> temp;
+        helper(num, temp, 0, n, k);
+        return res;
+    }
+};
+// @lc code=end
+
