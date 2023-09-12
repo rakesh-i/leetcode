f3263e3dccadcaa892ca05a68d7f7b45454db8ef
rakesh-i <rakesh.007.ac@gmail.com>
2023-09-12 13:14:53 +0530

    47/47 cases passed (33 ms) Your runtime beats 83.09 % of cpp submissions Your memory usage beats 73.97 % of cpp submissions (10.9 MB)

--- 1143.longest-common-subsequence.cpp
+++ 1143.longest-common-subsequence.cpp
@@ -7,20 +7,24 @@
 // @lc code=start
 class Solution {
 public:
-    int LCS(int n, int m, string &s1, string &s2, vector<vector<int>> &memo) {
-        if(n == 0 || m == 0) return 0;
-        if(memo[n][m]!=-1) return memo[n][m];
-        if(s1[n-1] == s2[m-1])
-            return memo[n][m] = 1 + LCS(n-1, m-1, s1, s2,memo);
-        else
-            return memo[n][m] = max(LCS(n, m-1, s1, s2,memo), LCS(n-1, m, s1, s2, memo));
-    } 
-    
+    int dp[1001][1001];
+    int helper(string &t1, string &t2, int m, int n){
+        if(m==0||n==0){
+            return 0;
+        }
+        if(dp[m][n]!=-1){
+            return dp[m][n];
+        }
+        if(t1[m-1]==t2[n-1]){
+            return dp[m][n] = 1+helper(t1, t2, m-1, n-1);
+        }
+        else{
+            return dp[m][n]=max(helper(t1, t2, m-1, n), helper(t1, t2, m, n-1));
+        }
+    }
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size();
-        int m = text2.size();
-        vector<vector<int>> memo(n+1, vector<int>(m+1, -1));
-        return LCS(n, m, text1, text2, memo);
+        memset(dp, -1, sizeof(dp));
+        return helper(text1, text2, text1.size(), text2.size());
     }
 };
 // @lc code=end
