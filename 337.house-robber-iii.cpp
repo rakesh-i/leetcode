25fa9e736bd042e2c883c5736064de30fca93e18
rakesh-i <rakesh.007.ac@gmail.com>
2023-09-27 11:38:27 +0530

    124/124 cases passed (8 ms) Your runtime beats 85.43 % of cpp submissions Your memory usage beats 69.91 % of cpp submissions (22.7 MB)

---
+++ 337.house-robber-iii.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=337 lang=cpp
+ *
+ * [337] House Robber III
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int helper(TreeNode* root, int &l, int &r){
+        if(!root){
+            return 0;
+        }
+        int ll=0, lr=0, rl=0, rr=0;
+        l = helper(root->left, ll, lr);
+        r = helper(root->right, rl, rr);
+        return max(root->val+ll+lr+rl+rr, l+r);
+    }
+    int rob(TreeNode* root) {
+        int l,r;
+        return helper(root, l, r);
+    }
+};
+// @lc code=end
+
