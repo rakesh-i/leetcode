8ebc4ed402a82b5e33373676a02443b7a876476a
rakesh-i <rakesh.007.ac@gmail.com>
2023-10-10 19:38:36 +0530

    92/92 cases passed (36 ms) Your runtime beats 47.3 % of cpp submissions Your memory usage beats 37.4 % of cpp submissions (13.7 MB)

--- 1140.stone-game-ii.cpp
+++ 1140.stone-game-ii.cpp
@@ -7,28 +7,29 @@
 // @lc code=start
 class Solution {
 public:
-    int helper(vector<int>&piles, int p, int i, int m, vector<vector<vector<int>>>& dp){
+    int helper(vector<int>& piles, int p, int i, int m, vector<vector<vector<int>>> &dp){
         if(i==piles.size()){
             return 0;
         }
         if(dp[p][i][m]!=-1){
             return dp[p][i][m];
         }
-        int res = p ==1?1000000:-1,s=0;
-        for(int x=1; x <= min(2*m, int(piles.size()-i)); x++){
-            s+=piles[i+x-1];
+        int res = p==1?1000000:0;
+        int sum = 0;
+        for(int x=1; x<=min(2*m, int(piles.size()-i)); x++){
+            sum+= piles[i+x-1];
             if(p==0){
-                res = max(res, s+helper(piles, 1, i+x, max(m,x), dp));
+                res = max(res, sum+helper(piles, 1, x+i, max(x, m), dp));
             }
             else{
-                res = min(res, helper(piles, 0, i+x, max(m, x), dp));
+                res = min(res, helper(piles, 0, x+i, max(x,m), dp));
             }
         }
         return dp[p][i][m] = res;
     }
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
-        vector dp(2, vector(n+1, vector<int>(n+1, -1)));
+        vector<vector<vector<int>>> dp(2, vector<vector<int>>(n+1, vector<int>(n+1, -1)));
         return helper(piles, 0, 0, 1, dp);
     }
 };
