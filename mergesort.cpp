f81894b1d660b58de2bec046fec79d927393ea90
rakesh-i <rakesh.007.ac@gmail.com>
2023-10-14 14:35:20 +0530

    testing

--- mergesort.cpp
+++ mergesort.cpp
@@ -19,22 +19,23 @@ void merge(vector<int> &a, int s, int mid, int e){
     for(int i=0; i<y; i++){
         two.push_back(a[mid+1+i]);
     }
-    int i=0, j=0, k=s;
-    while(i<x&&j<y){
-        if(one[i]<=two[j]){
-            a[k++]=one[i++];
-        }
-        else{
-            a[k++] = two[j++];
-        }
-    }
-    while(i<x){
-        a[k++] = one[i++];
-    }
-    while(j<y){
-        a[k++] = two[j++];
-    }
-    print(a);
+    print(one);
+    print(two);
+    // int i=0, j=0, k=s;
+    // while(i<x&&j<y){
+    //     if(one[i]<=two[j]){
+    //         a[k++]=one[i++];
+    //     }
+    //     else{
+    //         a[k++] = two[j++];
+    //     }
+    // }
+    // while(i<x){
+    //     a[k++] = one[i++];
+    // }
+    // while(j<y){
+    //     a[k++] = two[j++];
+    // }
 
 }
 
@@ -54,9 +55,9 @@ int main()
 {
     vector<int> a = {8,3,1,7,4,5,2,6};
     divide(a, 0, a.size()-1);
-    for(int i=0; i<a.size(); i++){
-        cout<<a[i]<<" ";
-    }
-    cout<<endl;
+    // for(int i=0; i<a.size(); i++){
+    //     cout<<a[i]<<" ";
+    // }
+    // cout<<endl;
     return 0;
 }
