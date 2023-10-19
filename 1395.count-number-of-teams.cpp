535b80429a17c4ee81df95977029f9612f12dc2c
rakesh-i <rakesh.007.ac@gmail.com>
2023-10-19 12:11:22 +0530

    72/72 cases passed (116 ms) Your runtime beats 49.4 % of cpp submissions Your memory usage beats 65.01 % of cpp submissions (9.2 MB)

---
+++ 1395.count-number-of-teams.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=1395 lang=cpp
+ *
+ * [1395] Count Number of Teams
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int numTeams(vector<int> &rating)
+    {
+        int n = rating.size();
+        int res = 0;
+        for (int i = 1; i < n - 1; i++)
+        {
+            int ll = 0, lr = 0, gr = 0, gl = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (rating[i] > rating[j])
+                {
+                    if (i > j)
+                    {
+                        lr++;
+                    }
+                    else
+                    {
+                        ll++;
+                    }
+                }
+                if (rating[i] < rating[j])
+                {
+                    if (i > j)
+                    {
+                        gr++;
+                    }
+                    else
+                    {
+                        gl++;
+                    }
+                }
+            }
+            res += gl * lr + gr * ll;
+        }
+        return res;
+    }
+};
+// @lc code=end
