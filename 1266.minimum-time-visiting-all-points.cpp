9611f88a185eeceb0e342ee3f3949f45b5ce375f
rakesh-i <rakesh.007.ac@gmail.com>
2023-12-07 11:49:26 +0530

    122/122 cases passed (0 ms) Your runtime beats 100 % of cpp submissions Your memory usage beats 69.53 % of cpp submissions (10.4 MB)

---
+++ 1266.minimum-time-visiting-all-points.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=1266 lang=cpp
+ *
+ * [1266] Minimum Time Visiting All Points
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>> &points)
+    {
+        int ans = 0;
+        for (int i = 1; i < points.size(); i++)
+        {
+            ans += max(abs(points[i][1] - points[i - 1][1]), abs(points[i][0] - points[i - 1][0]));
+        }
+        return ans;
+    }
+};
+// @lc code=end
